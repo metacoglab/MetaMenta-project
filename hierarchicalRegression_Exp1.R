b655df6eb26099c137712a383580a793a6e1eac7
Steve Fleming <fleming.sm@gmail.com>
2023-08-10 09:50:39 +0100

    Updates to regression code for Exp 1

--- hierarchicalRegression_Exp1.R
+++ hierarchicalRegression_Exp1.R
@@ -1,5 +1,6 @@
 ## cleans up and analyses online prolific data (experiment 1)
-## elisavanderplasATgmail.com
+## Original code by elisavanderplasATgmail.com
+## Updated August 2023 by stephen.flemingATucl.ac.uk
 
 rm(list=ls())
 require(R.matlab) 
@@ -11,13 +12,15 @@ require(plyr)
 require(readr)#to easily read csv's
 options(contrasts = c("contr.treatment", "contr.poly")) # This is R defaults but set it anyway to be safe
 
+### 1) LOAD DATA
+
 dat=NULL #initiate vars to load
 fullIDs=NULL
 sessions = c('v38', 'v39', 'v40', 'v41', 'v42', 'v43')
 #load in all task data
 for (i in 1:length(sessions)){ 
   currentData = paste('data_exp_12022-',sessions[i],sep="")
-  dataDir = "~/Dropbox/MetaMenta-project/Data/Exp1/" 
+  dataDir = "~/Dropbox/InPrep/Autism/Code/MetaMenta-project/Data/Exp1/" 
   Dir = paste(dataDir,currentData, '/', sep="")
   files = c('_task-pf6t', '_task-yzt9')
   for (j in 1:length(files)){
@@ -32,6 +35,7 @@ for (i in 1:length(sessions)){
 setwd(dataDir)
 subData <- read_csv('data.csv')
 
+## 2) EXTRACT DATA AND PUT IN BIG ARRAY
 #prepare the meta-task data
 fullIDs = t(fullIDs)
 bigData = NULL
@@ -50,7 +54,7 @@ for (s in 1:length(fullIDs))
   acc[is.na(acc)]=0##accuracy==1: correct, accuracy==0: wrong
   acc = acc-0.5##accuracy==0.5: correct, accuracy==-0.5: wrong
   
-  #have to ecompute objectively correct answer because js script doesn't give that 
+  #have to compute objectively correct answer because js script doesn't give that 
   dir=rep(1, length(acc))
   for (t in 1:length(acc)){
     if (acc[t] ==1 & keypress[t] == 87){
@@ -65,8 +69,9 @@ for (s in 1:length(fullIDs))
   RAADS = rep(subData$RAADS[s], length(acc))
   AQ_comm = rep(subData$AQ10_C[s], length(acc))
   RAADS_menta = rep(subData$RAADS_MENTA[s], length(acc))
+  MCQ_cat = rep(subData$MCQ_cat[s], length(acc))
   MCQ_emos = rep(subData$MCQ_feelings[s], length(acc))
-  subData1 = data.frame("subj"=subj, "dir"=dir,"AQ10"=AQ,"RAADS14"= RAADS,"AQ10_comm"=AQ_comm,"RAADS14_menta"= RAADS_menta, "acc"=acc, "conf"=conf, "logRT"=logRT, "MCQ_emo"=MCQ_emos)
+  subData1 = data.frame("subj"=subj, "dir"=dir,"AQ10"=AQ,"RAADS14"= RAADS,"AQ10_comm"=AQ_comm,"RAADS14_menta"= RAADS_menta, "acc"=acc, "conf"=conf, "logRT"=logRT, "MCQ_emo"=MCQ_emos, "MCQ_cat"=MCQ_cat)
     
   #add to larger file 
   bigData = rbind(bigData, subData1)
@@ -76,21 +81,42 @@ bigData$subj <- factor(bigData$subj)
 # skip rows with NaNs
 bigData_clean <- na.omit(bigData)
 
-# get median-split on RAADS
-RAADS_med <- median(bigData_clean$RAADS14)
-bigData_clean$RAADSbi = cut(as.numeric(bigData_clean$RAADS14),breaks=c(0,RAADS_med,42),include.lowest=T, labels=c("low", "high"))
-bigData_clean$RAADSbi <- factor(bigData_clean$RAADSbi)
+## 3) COMPUTE INTERACTIONS BETWEEN RT-CONF EFFECT AND MCQ SCORES 
 
-## distinguish between error/correct & high vs. low autism trials
-bigData_err <- bigData_clean[bigData_clean$acc == -0.5, ]
-bigData_corr <- bigData_clean[bigData_clean$acc == 0.5, ]
-bigData_lASDerr <- bigData_err[bigData_err$RAADSbi == "low", ]
-bigData_hASDerr <- bigData_err[bigData_err$RAADSbi == "high", ]
-bigData_lASDcorr <- bigData_corr[bigData_corr$RAADSbi == "low", ]
-bigData_hASDcorr <- bigData_corr[bigData_corr$RAADSbi == "high", ]
+## 3a) MCQ-cat 
 
-##MCQ_emo
-##conduct a hierarchical regression to see if interaction w/ MCQ can explain how much confidence is influenced by standardized log RT
+confModel_noMCQ = lmer(conf ~ acc + logRT + acc * logRT + (1 + acc + logRT|subj), data=bigData_clean
+                       , control = lmerControl(optimizer = "optimx", calc.derivs = FALSE, optCtrl = list(method = "bobyqa", starttests = FALSE, kkt = FALSE, REML = FALSE)))
+
+confModel_wMCQ = lmer(conf ~ MCQ_cat*(acc + logRT + acc * logRT) + (1 + acc + logRT|subj), data=bigData_clean
+                      , control = lmerControl(optimizer = "optimx", calc.derivs = FALSE, optCtrl = list(method = "bobyqa", starttests = FALSE, kkt = FALSE, REML = FALSE)))
+
+fix <- fixef(confModel_wMCQ)
+print(summary(confModel_wMCQ))
+print(Anova(confModel_wMCQ, type = 3))
+coef(summary(confModel_wMCQ)) #get the contrast statistics
+fix.se <- sqrt(diag(vcov(confModel_wMCQ))) 
+
+## check if including MCQ as interaction improved the fit of the model, line 257
+anova(confModel_noMCQ,confModel_wMCQ) 
+
+# get median MCQ
+MCQ_cat_med <- median(bigData_clean$MCQ_cat)
+bigData_clean$MCQcat_bi = cut(as.numeric(bigData_clean$MCQ_cat),breaks=c(0,MCQ_cat_med,max(bigData_clean$MCQ_cat)),include.lowest=T, labels=c("low", "high"))
+bigData_clean$MCQcat_bi <- factor(bigData_clean$MCQcat_bi)
+
+#make a nice figure of conf~RTxMCQ interaction 
+ggplot(bigData_clean, aes(x=logRT, y=conf, colour=MCQcat_bi)) + 
+  geom_count() + 
+  scale_color_manual(values=c("salmon", "turquoise3")) +
+  geom_point(shape=19, size=0.5, alpha = 1.0) + 
+  geom_smooth(method="lm", se = T, aes(fill=MCQcat_bi), alpha = 0.2) + 
+  labs(y="Confidence", x = "logRT (z-score)", color = "MCQ-cat") + 
+  theme_minimal() + theme(axis.text=element_text(size=18),axis.title=element_text(size=25))
+
+## 3b) MCQ-feelings
+
+#conduct a hierarchical regression to see if interaction w/ MCQ can explain how much confidence is influenced by standardized log RT
 confModel_noMCQ = lmer(conf ~ acc + logRT + acc * logRT + (1 + acc + logRT|subj), data=bigData_clean
                       , control = lmerControl(optimizer = "optimx", calc.derivs = FALSE, optCtrl = list(method = "bobyqa", starttests = FALSE, kkt = FALSE, REML = FALSE)))
 
@@ -107,20 +133,21 @@ fix.se <- sqrt(diag(vcov(confModel_wMCQ)))
 anova(confModel_noMCQ,confModel_wMCQ) 
 
 # get median MCQ
-MCQ_med <- median(bigData_clean$MCQ_emo)
-bigData_clean$MCQbi = cut(as.numeric(bigData_clean$MCQ_emo),breaks=c(0,MCQ_med,max(bigData_clean$MCQ_emo)),include.lowest=T, labels=c("low", "high"))
-bigData_clean$MCQbi <- factor(bigData_clean$MCQbi)
+MCQ_emo_med <- median(bigData_clean$MCQ_emo)
+bigData_clean$MCQemo_bi = cut(as.numeric(bigData_clean$MCQ_emo),breaks=c(0,MCQ_emo_med,max(bigData_clean$MCQ_emo)),include.lowest=T, labels=c("low", "high"))
+bigData_clean$MCQemo_bi <- factor(bigData_clean$MCQemo_bi)
 
-#make a nice figure of conf~RTxRAADS interaction 
-ggplot(bigData_clean, aes(x=logRT, y=conf, colour=MCQbi)) + 
+#make a nice figure of conf~RTxMCQ interaction 
+ggplot(bigData_clean, aes(x=logRT, y=conf, colour=MCQemo_bi)) + 
   geom_count() + 
   scale_color_manual(values=c("salmon", "turquoise3")) +
   geom_point(shape=19, size=0.5, alpha = 1.0) + 
-  geom_smooth(method="lm", se = T, aes(fill=MCQbi), alpha = 0.2) + 
-  labs(y="Confidence", x = "logRT (z-score)", color = "Mentalizing efficiency") + 
+  geom_smooth(method="lm", se = T, aes(fill=MCQemo_bi), alpha = 0.2) + 
+  labs(y="Confidence", x = "logRT (z-score)", color = "MCQ-feelings") + 
   theme_minimal() + theme(axis.text=element_text(size=18),axis.title=element_text(size=25))
 
-##RAADS
+## 4) EXAMINE INTERACTIONS WITH RAADS
+
 ##conduct a hierarchical regression to see if interaction w/ RAADS can explain how much confidence is influenced by standardized log RT
 confModel_noRAADS = lmer(conf ~ acc + logRT + acc * logRT + (1 + acc + logRT|subj), data=bigData_clean
                          , control = lmerControl(optimizer = "optimx", calc.derivs = FALSE, optCtrl = list(method = "bobyqa", starttests = FALSE, kkt = FALSE)))
@@ -137,7 +164,24 @@ fix.se <- sqrt(diag(vcov(confModel_wRAADS)))
 ## check if including RAADS as interaction improved the fit of the model 
 anova(confModel_noRAADS,confModel_wRAADS) 
 
-##get the beta coefficients for correct/err, for Figure 2a
+## 4) EXTRACT CORRECT/ERROR BETAS FOR PLOTTING (NOT USED)
+
+# get median-split on RAADS
+bigData_clean$RAADS14 = bigData_clean$RAADS14 - 14 # correct RAADS-14 scores into units of 0-3 on each question to match previous literature 
+bigData_clean$RAADS14_menta = bigData_clean$RAADS14_menta - 7 # correct RAADS-14 scores into units of 0-3 on each question to match previous literature 
+RAADS_med <- median(bigData_clean$RAADS14)
+bigData_clean$RAADSbi = cut(as.numeric(bigData_clean$RAADS14),breaks=c(0,RAADS_med,max(bigData_clean$RAADS14)),include.lowest=T, labels=c("low", "high"))
+bigData_clean$RAADSbi <- factor(bigData_clean$RAADSbi)
+
+## distinguish between error/correct & high vs. low autism trials
+bigData_err <- bigData_clean[bigData_clean$acc == -0.5, ]
+bigData_corr <- bigData_clean[bigData_clean$acc == 0.5, ]
+bigData_lASDerr <- bigData_err[bigData_err$RAADSbi == "low", ]
+bigData_hASDerr <- bigData_err[bigData_err$RAADSbi == "high", ]
+bigData_lASDcorr <- bigData_corr[bigData_corr$RAADSbi == "low", ]
+bigData_hASDcorr <- bigData_corr[bigData_corr$RAADSbi == "high", ]
+
+##get the beta coefficients for correct/err
 corr_lASD = lmer(conf ~ logRT + (1 + logRT|subj), data=bigData_lASDcorr,
                  control = lmerControl(optimizer = "optimx", calc.derivs = FALSE, optCtrl = list(method = "bobyqa", starttests = FALSE, kkt = FALSE)))
 fix <- fixef(corr_lASD)
@@ -169,5 +213,4 @@ fix.se <- sqrt(diag(vcov(err_hASD)))
 betas <- c(fix, fix.se)
 setwd(dataDir)
 write.csv(betas, file = paste('err_hASD.csv'))
-
 
